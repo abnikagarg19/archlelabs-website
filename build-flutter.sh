8c6e71f4687d6c42475a71186248675bd3439f5e
Abnika Garg <83591484+AbnikaGarg@users.noreply.github.com>
2025-03-01 23:25:46 +0530

    .

--- build-flutter.sh
+++ build-flutter.sh
@@ -2,13 +2,3 @@
 
 # Flutter build commands for web release
 flutter/bin/flutter build web --release
-cd flutter
-if git pull; then
-  cd ..
-else
-  git clone https://github.com/flutter/flutter.git
-fi
-ls
-flutter/bin/flutter doctor
-flutter/bin/flutter clean
-flutter/bin/flutter config --enable-web true
